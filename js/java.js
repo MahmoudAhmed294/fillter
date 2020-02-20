5a9d8b28589f8785a06bfe0df75d094c7647a47c
mahmoud ahmed <mahmoudelnagar29497@gmail.com>
2020-02-20 05:00:40 -0800

    Fillter

---
+++ js/java.js
@@ -0,0 +1,103 @@
+
+ function btn_all(){
+    document.querySelector('.continer .continer_imgs').innerHTML="";
+
+    for(let x=1;x<7;x++){
+let page_all=document.querySelector('.continer .continer_imgs ').innerHTML +=`<div class="img_content">
+<div class="img_hover">  <img src="img/${x}.jpg" ></div>
+  <div class="projact_name"><h1>Projact Name</h1></div>
+  <div class="hover_1_link"></div>
+  <div class="social_media_icons">
+
+      <ul>
+       <li><a href="#"><i class="fab fa-twitter"></i></a></li>  
+       <li><a href="#"><i class="fab fa-facebook-f"></a></i></i></li>  
+       <li><a href="#"><i class="fab fa-google-plus-g"></a></i></li>  
+      </ul>
+  </div>
+</div>`
+    }
+    
+    
+}
+btn_all();
+var page_Design;
+function btn_Design(){
+    document.querySelector('.continer .continer_imgs').innerHTML="";
+    for(let x=6;x<9;x++){
+       page_Design= document.querySelector('.continer .continer_imgs ').innerHTML +=`<div class="img_content">
+        <div class="img_hover">  <img src="img/${x}.jpg" ></div>
+  <div class="projact_name"><h1>Projact Name</h1></div>
+  <div class="hover_1_link"></div>
+  <div class="social_media_icons">
+
+  <ul>
+  <li><a href="#"><i class="fab fa-twitter"></i></a></li>  
+  <li><a href="#"><i class="fab fa-facebook-f"></a></i></i></li>  
+  <li><a href="#"><i class="fab fa-google-plus-g"></a></i></li>  
+  </ul>
+  </div>
+  </div>`
+    }
+    
+    
+}
+function btn_Develop(){
+    document.querySelector('.continer .continer_imgs').innerHTML="";
+    for(let x=10;x<12;x++){
+       page_Design= document.querySelector('.continer .continer_imgs ').innerHTML +=`<div class="img_content">
+        <div class="img_hover">  <img src="img/${x}.jpg" ></div>
+  <div class="projact_name"><h1>Projact Name</h1></div>
+  <div class="hover_1_link"></div>
+  <div class="social_media_icons">
+
+  <ul>
+  <li><a href="#"><i class="fab fa-twitter"></i></a></li>  
+  <li><a href="#"><i class="fab fa-facebook-f"></a></i></i></li>  
+  <li><a href="#"><i class="fab fa-google-plus-g"></a></i></li>  
+  </ul>
+  </div>
+  </div>`
+    }
+    
+    
+}
+function btn_Graphic(){
+    document.querySelector('.continer .continer_imgs').innerHTML="";
+    for(let x=11;x<13;x++){
+       page_Design= document.querySelector('.continer .continer_imgs ').innerHTML +=`<div class="img_content">
+        <div class="img_hover">  <img src="img/${x}.jpg" ></div>
+  <div class="projact_name"><h1>Projact Name</h1></div>
+  <div class="hover_1_link"></div>
+  <div class="social_media_icons">
+
+  <ul>
+  <li><a href="#"><i class="fab fa-twitter"></i></a></li>  
+  <li><a href="#"><i class="fab fa-facebook-f"></a></i></i></li>  
+  <li><a href="#"><i class="fab fa-google-plus-g"></a></i></li>  
+  </ul>
+  </div>
+  </div>`
+    }
+    
+    
+}
+function btn_Mobile(){
+    document.querySelector('.continer .continer_imgs').innerHTML="";
+       page_Design= document.querySelector('.continer .continer_imgs ').innerHTML +=`<div class="img_content">
+        <div class="img_hover">  <img src="img/12.jpg" ></div>
+  <div class="projact_name"><h1>Projact Name</h1></div>
+  <div class="hover_1_link"></div>
+  <div class="social_media_icons">
+
+  <ul>
+  <li><a href="#"><i class="fab fa-twitter"></i></a></li>  
+  <li><a href="#"><i class="fab fa-facebook-f"></a></i></i></li>  
+  <li><a href="#"><i class="fab fa-google-plus-g"></a></i></li>  
+  </ul>
+  </div>
+  </div>`
+    
+    
+    
+}
